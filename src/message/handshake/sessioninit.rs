cd08cc32a83904e4285e6498f44d7b15fcc9f681
Max Audron <audron@cocaine.farm>
2025-03-05 14:48:57 +0100

    finish up SessionInit

--- src/message/handshake/sessioninit.rs
+++ src/message/handshake/sessioninit.rs
@@ -1,10 +1,11 @@
-use crate::error::ProtocolError;
+use crate::error::{ProtocolError, Result};
 use crate::message::objects::Identity;
+use crate::message::NetworkMap;
 use crate::primitive::{BufferInfo, NetworkId, Variant, VariantList, VariantMap};
 use crate::HandshakeSerialize;
 
 /// SessionInit is received along with ClientLoginAck to initialize that user Session
-// TODO Replace with proper types
+/// Upon receiving this the client needs to send [InitRequest] for the Networks using the NetworkId
 #[derive(Debug, Clone)]
 pub struct SessionInit {
     /// List of all configured identities
@@ -18,7 +19,7 @@ pub struct SessionInit {
 impl TryFrom<VariantMap> for SessionInit {
     type Error = ProtocolError;
 
-    fn try_from(input: VariantMap) -> Result<Self, Self::Error> {
+    fn try_from(input: VariantMap) -> Result<Self> {
         let mut state: VariantMap = input
             .get("SessionState")
             .ok_or_else(|| ProtocolError::MissingField("SessionState".to_string()))?
@@ -43,38 +44,38 @@ impl TryFrom<VariantMap> for SessionInit {
             identities: identities
                 .into_iter()
                 .map(|x| x.try_into())
-                .collect::<Result<Vec<_>, _>>()?,
+                .collect::<Result<Vec<_>>>()?,
             buffers: buffers
                 .iter()
                 .map(|buffer| match buffer {
                     Variant::BufferInfo(buffer) => Ok(buffer.clone()),
                     _ => Err(ProtocolError::WrongVariant),
                 })
-                .collect::<Result<Vec<_>, _>>()?,
+                .collect::<Result<Vec<_>>>()?,
             network_ids: network_ids
                 .iter()
                 .map(|network| match network {
                     Variant::NetworkId(network) => Ok(*network),
                     _ => Err(ProtocolError::WrongVariant),
                 })
-                .collect::<Result<Vec<_>, _>>()?,
+                .collect::<Result<Vec<_>>>()?,
         })
     }
 }
 
 impl HandshakeSerialize for SessionInit {
-    fn serialize(&self) -> Result<Vec<u8>, ProtocolError> {
+    fn serialize(&self) -> Result<Vec<u8>> {
         let mut values: VariantMap = VariantMap::with_capacity(4);
         values.insert("MsgType".to_string(), Variant::String("SessionInit".to_string()));
-        // values.insert(
-        //     "Identities".to_string(),
-        //     Variant::VariantList(
-        //         self.identities
-        //             .iter()
-        //             .map(|ident| Variant::VariantMap(ident.clone().into()))
-        //             .collect(),
-        //     ),
-        // );
+        values.insert(
+            "Identities".to_string(),
+            Variant::VariantList(
+                self.identities
+                    .iter()
+                    .map(|ident| -> Result<Variant> { Ok(Variant::VariantMap(ident.to_network_map()?)) })
+                    .collect::<Result<Vec<Variant>>>()?,
+            ),
+        );
         values.insert(
             "BufferInfos".to_string(),
             Variant::VariantList(
